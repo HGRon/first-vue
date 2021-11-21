ee0505c0320fbd63b4f6679c1bc3b4cf195f9ba9
HGRon <higordasilvalins@gmail.com>
2021-11-21 01:14:10 -0300

    update(*): update to create and load components in main page

---
+++ src/page/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <SystemBar></SystemBar>
+    <Header></Header>
+    <Navbar></Navbar>
+    <Footer></Footer>
+    <BottomNavigation></BottomNavigation>
+  </div>
+</template>
+
+<script>
+  import BottomNavigation from '/src/components/BottomNavigation'
+  import SystemBar from '/src/components/SystemBar'
+  import Footer from '/src/components/Footer';
+  import Navbar from '/src/components/Navbar';
+  import Header from '/src/components/Header'
+
+  export default {
+    name: 'Main',
+
+    components: {
+      Header,
+      Footer,
+      Navbar,
+      SystemBar,
+      BottomNavigation
+    },
+
+    data: () => ({}),
+  }
+</script>
