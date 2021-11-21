5275fe9982d2cb8a52b4158aadaf8e700d05f977
HGRon <higordasilvalins@gmail.com>
2021-11-21 01:13:24 -0300

    feat(SystemBar): create system bar to application

---
+++ src/components/SystemBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-system-bar dark>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-wifi-strength-4</v-icon>
+      <v-icon>mdi-signal-cellular-outline</v-icon>
+      <v-icon>mdi-battery</v-icon>
+      <span>12:30</span>
+    </v-system-bar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SystemBar',
+  }
+</script>
+
+<style scoped>
+
+</style>
