9dbd7735870f51ad5eb9961f07fe2eacbf4befb0
HGRon <higordasilvalins@gmail.com>
2021-11-28 17:06:34 -0300

    update(App): update to add router to App page

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,52 @@
 <template>
   <v-app>
+    <SystemBar></SystemBar>
+    <Header></Header>
     <v-main>
-      <Main/>
+      <router-view to="/"></router-view>
     </v-main>
+    <Footer></Footer>
   </v-app>
 </template>
 
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+
 <script>
 
-import Main from './page/Main';
+import SystemBar from '/src/components/SystemBar'
+import Footer from '/src/components/Footer';
+import Header from '/src/components/Header'
 
 export default {
-  name: 'App',
+  name: 'Main',
 
   components: {
-    Main,
+    Header,
+    Footer,
+    SystemBar,
   },
 
-  data: () => ({
-    //
-  }),
-};
-
+  data: () => ({}),
+}
 </script>
