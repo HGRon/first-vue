19343f0ba14915431347a1ffa844c2bef825c1b0
HGRon <higordasilvalins@gmail.com>
2021-11-28 17:11:11 -0300

    update(Footer): update to change style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,13 +2,14 @@
   <v-footer
       dark
       padless
+      color="white"
   >
-    <v-card flat tile class="lighten-1 white--text text-center">
+    <v-card flat tile class="lighten-1 text-center">
       <v-card-text style="width: 100vw">
         <v-btn
             v-for="icon in icons"
             :key="icon"
-            class="mx-4 white--text"
+            class="mx-4"
             icon
         >
           <v-icon size="24px">
