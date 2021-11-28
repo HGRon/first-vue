8d99053493842533fe150d9801ac2c03dc9db52c
HGRon <higordasilvalins@gmail.com>
2021-11-28 17:10:49 -0300

    update(Header): update header style to navigate by screens

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,47 +1,82 @@
 <template>
   <div>
-    <v-app-bar
-        color="accent-4"
-        dense
-        dark
-    >
+    <v-toolbar class="background">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Firs Vue</v-toolbar-title>
+      <v-toolbar-title>Des. Web</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+      <div
+          v-for="item in items"
+          :key="item.title"
+          link>
+        <v-btn text>
+          <v-icon @click="goTo(item.type)">{{ item.icon }}</v-icon>
+        </v-btn>
+      </div>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80"></v-img>
+        </v-list-item-avatar>
 
-      <v-menu
-          left
-          bottom
-      >
-        <v-list>
-          <v-list-item
-              v-for="n in 5"
-              :key="n"
-              @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
+        <v-list-item-content>
+          <v-list-item-title>Joséfino</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+        >
+          <v-list-item-icon @click="goTo(item.type)">
+            <v-icon class="pr-2">{{ item.icon }} </v-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-icon>
+
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  data () {
+    return {
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'mdi-home', type: 'home' },
+        { title: 'Sobre', icon: 'mdi-forum', type: 'about'},
+        { title: 'Produtos', icon: 'mdi-archive', type: 'products'},
+        { title: 'Usuarios', icon: 'mdi-account', type: 'users'},
+      ],
+    }
+  },
+  methods:{
+    goTo(name){
+      this.$router.push(name)
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
+
+.background {
+  background: white !important;
+  box-shadow: 0 5px 16px #00000070;
+}
 
 </style>
