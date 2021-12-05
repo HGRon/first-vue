637c59637bb9f82af01bd42389e5c2848615df75
HGRon <higordasilvalins@gmail.com>
2021-12-05 19:30:00 -0300

    update(package): update to change user card template

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,36 +1,50 @@
 <template>
   <v-card
       class="mx-auto"
-      color="#26c6da"
-      dark
-      max-width="400"
+      max-width="344"
   >
+    <v-img
+        :src="image"
+        height="200px"
+    ></v-img>
+
+    <v-card-title>
+      {{ name }}
+    </v-card-title>
+      Cadastrado à {{ likes }} anos
+    <v-card-subtitle>
+
+    </v-card-subtitle>
+
     <v-card-actions>
-      <v-list-item class="grow">
-        <v-list-item-avatar color="grey darken-3">
-          <v-img
-              class="elevation-6"
-              alt=""
-              src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-          ></v-img>
-        </v-list-item-avatar>
+      <v-btn
+          @click="show = !show"
+          color="orange lighten-2"
+          text
+      >
+        More
+      </v-btn>
 
-        <v-list-item-content>
-          <v-list-item-title>{{ name }}</v-list-item-title>
-        </v-list-item-content>
+      <v-spacer></v-spacer>
 
-        <v-row
-            align="center"
-            justify="end"
-        >
-          <v-icon class="mr-1">
-            mdi-heart
-          </v-icon>
-          <span class="subheading mr-2">256</span>
-          <span class="mr-1">·</span>
-        </v-row>
-      </v-list-item>
+      <v-btn
+          icon
+          @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
     </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+            <strong>Email:</strong> {{ email }} <br>
+            <strong>Phone:</strong> {{ phone }} <br>
+        </v-card-text>
+      </div>
+    </v-expand-transition>
   </v-card>
 </template>
 
@@ -40,8 +54,15 @@ export default {
 
   props: [
     "name",
+    "image",
+    "likes",
+    "email",
+    "phone"
   ],
 
+  data: () => ({
+    show: false,
+  }),
 }
 </script>
 
